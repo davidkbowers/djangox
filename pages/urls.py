8474b18b4fb1f70667dbedf714579917b1bea6e9
David K Bowers <dbowers@bowerstechnologies.com>
2024-06-12 21:23:40 -0400

    added a bunch of empty pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, BlogPageView, ClassesPageView, CommunityPageView, ContactPageView, ShopPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("blog/", BlogPageView.as_view(), name="blog"),
+    path("classes/", ClassesPageView.as_view(), name="classes"),  
+    path("community/", CommunityPageView.as_view(), name="community"),
+    path("contact/", ContactPageView.as_view(), name="contact"),   
+    path("shop/", ShopPageView.as_view(), name="shop"), 
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,23 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+
+class BlogPageView(TemplateView):
+    template_name = "pages/blog.html" 
+
+
+class ClassesPageView(TemplateView):
+    template_name = "pages/classes.html"  
+
+
+class CommunityPageView(TemplateView):
+    template_name = "pages/community.html"  
+
+
+class ContactPageView(TemplateView):
+    template_name = "pages/contact.html"   
+
+
+class ShopPageView(TemplateView):
+    template_name = "pages/shop.html"  
